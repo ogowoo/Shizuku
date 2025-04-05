68162cb7ea2f0e8010a301a3131c04774a8de5d9
ogowoo <ogowoo@gmail.com>
2025-04-05 11:25:09 +0800

    Update ShizukuService.java

--- server/src/main/java/rikka/shizuku/server/ShizukuService.java
+++ server/src/main/java/rikka/shizuku/server/ShizukuService.java
@@ -549,7 +549,7 @@ public class ShizukuService extends Service<ShizukuUserServiceManager, ShizukuCl
             }
 
             Bundle extra = new Bundle();
-            extra.putParcelable("moe.shizuku.privileged.intent.extra.BINDER", new BinderContainer(binder));
+            extra.putParcelable("moe.shizuku.privileged.api.intent.extra.BINDER", new BinderContainer(binder));
 
             Bundle reply = IContentProviderUtils.callCompat(provider, null, name, "sendBinder", null, extra);
             if (reply != null) {
